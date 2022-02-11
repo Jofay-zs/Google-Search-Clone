ab1bb29bd552d64e7831d3b4af56cc427d88ce53
jofay-zs <jofayzs19@gmail.com>
2022-02-10 22:40:42 -0500

    Creating a context to call the endpoints and testing it

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import "./App.css";
 import { useState } from "react";
 import { ThemeContext } from "./context/Theme";
 import { Route, Routes } from "react-router-dom";
-import General from "./Layouts/General";
+import General from "./layouts/General";
 import Results from "./pages/Results";
+import { ResultContextProvider } from "./context/Result";
 
 function App() {
   const [isTheme, setIsTheme] = useState(false);
   return (
-    <ThemeContext.Provider value={{isTheme, setIsTheme}}>
-      <Routes>
-        <Route path="/" element={<General />}>
-          <Route index element={<Results />} />
-          <Route path="search" element={<Results />} />
-          <Route path="images" element={<Results />} />
-          <Route path="news" element={<Results />} />
-          <Route path="videos" element={<Results />} />
-        </Route>
-      </Routes>
+    <ThemeContext.Provider value={{ isTheme, setIsTheme }}>
+      <ResultContextProvider>
+        <Routes>
+          <Route path="/" element={<General />}>
+            <Route index element={<Results />} />
+            <Route path="images" element={<Results />} />
+            <Route path="news" element={<Results />} />
+            <Route path="videos" element={<Results />} />
+          </Route>
+        </Routes>
+      </ResultContextProvider>
     </ThemeContext.Provider>
   );
 }
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,9 +1,52 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useResultContext } from "../context/Result";
+import Loading from "../components/Loading";
+import ReactPlayer from "react-player";
 
 const Results = () => {
   const location = useLocation();
-  return <main className="">{location.pathname}</main>;
+  const { getResults, results, searchTerm, isLoading } = useResultContext();
+
+  useEffect(() => {
+    getResults("/search/q=javascript&num=10");
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  switch (location.pathname) {
+    case "/":
+      return (
+        <div className="grid grid-cols-2">
+          <section className="col-span-2 lg:col-span-1">
+            {results?.results?.map(({ link, title, description }, index) => (
+              <article key={index} className="my-3 w-full">
+                <a href={link} rel="noreferrer">
+                  <span className="text-sm">
+                    {link.length > 30 ? link.substring(0, 30) : link}
+                  </span>
+                  <h6 className="text-xl hover:underline text-gray-600 dark:text-gray-400">
+                    {title}
+                  </h6>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    {description}
+                  </p>
+                </a>
+              </article>
+            ))}
+          </section>
+        </div>
+      );
+    case "/images":
+      return "IMAGES";
+    case "/news":
+      return "NEWS";
+    case "/videos":
+      return "VIDEOS";
+    default:
+      return "Error";
+  }
 };
 
 export default Results;
